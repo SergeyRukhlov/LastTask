bc62fea613d115c952cab80212a717575e69864e
Sergey Rukhlov <rukhlov-s@mail.ru>
2022-07-25 19:45:07 +0300

    lastTask1

---
+++ LastTask1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все
+//натуральные числа в промежутке от M до N.
+
+namespace GB
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Clear();
+            int n = InputNumbers(" Введите n: ");
+            int count = 2;
+            PrintNumber(n, count);
+            Console.Write(1);
+
+            void PrintNumber(int n, int count)
+            {
+                if (count > n)
+                    return;
+                PrintNumber(n, count + 1);
+                Console.Write(count + ", ");
+            }
+
+            int InputNumbers(string input)
+            {
+                Console.Write(input);
+                int output = Convert.ToInt32(Console.ReadLine());
+                return output;
+            }
+        }
+    }
+}
